0072f0c3a9b50978286b9e10b06a690aa8dbc8d3
VadymMakohon <errorks7@gmail.com>
2024-05-31 10:41:39 +0200

    updated style of the app

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // jQuery Code
 function newItem() {
-//1. Adding a new item to the list
+  //1. Adding a new item to the list
   let li = $('<li></li>');
   let inputValue = $('#input').val();
   li.append(inputValue);
@@ -11,21 +11,21 @@ function newItem() {
     $('#list').append(li);
   }
 
-//2. Crossing an item out
+  //2. Crossing an item out
   function crossOut() {
-		li.toggleClass("strike");
-	}
+    li.toggleClass("strike");
+  }
 
-	li.on("dblclick", function crossOut() {
-		li.toggleClass("strike");
-	});
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
 
-//3. Adding a delete button
+  //3. Adding a delete button
   let crossOutButton = $("<crossOutButton></crossOutButton>");
   $(crossOutButton).append("X");
   li.append(crossOutButton);
 
-//4. Adding CLASS DELETE (DISPLAY: NONE) from the css
+  //4. Adding CLASS DELETE (DISPLAY: NONE) from the css
   crossOutButton.on("click", function () {
     li.addClass("delete");
   });
